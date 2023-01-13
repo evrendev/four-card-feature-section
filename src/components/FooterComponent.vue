29b15b1a834aa7aae013557cea31998cce28ac5f
Evren Yeniev <mail@evren.dev>
2023-01-13 11:52:32 +0300

    Attributon moved to footer component

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer>
+    <div class="attribution" role="contentinfo">
+      Challenge by
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
+      Coded by <a href="https://github.com/evrendev" target="_blank">Evren Yeniev</a>.
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "FooterComponent",
+};
+</script>
+
+<style lang="scss" scoped>
+footer {
+  .attribution {
+    margin-top: 2rem;
+    font-size: 11px;
+    text-align: center;
+
+    a {
+      color: hsl(228, 45%, 44%);
+    }
+  }
+}
+</style>
